fc52f7bdebefa8cdee0ed3121c26fdc4c30c6aae
Arpit-singh12 <singharpit20034@gmail.com>
2025-05-31 00:21:56 +0530

    Implemented chat message section

--- Client/src/components/ChatContainer.jsx
+++ Client/src/components/ChatContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import assets from '../assets/assets';
+import assets, { messagesDummyData } from '../assets/assets';
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
     return selectedUser ? (
         <div className="h-full overflow-scroll relative backdrop-blur-lg">
+
+            {/*...Header of chat section...*/}
+
             <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
                 <img src={assets.profile_Arpit} alt="" className='w-8 rounded-full' />
                 <p className="flex-1 text-lg text-white flex items-center gap-2">
@@ -12,6 +15,18 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
                 </p>
                 <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
                 <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5' />
+            </div>
+            {/*...Chat messages section...*/}
+            <div className="flex flex-col h-[calc(100%-120%)] overflow-y-scroll p-3 pb-6">
+                {messagesDummyData.map((msg, index) => (
+                    <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f5116f10f3cd28382ed02' && 'flex-row-reverse'} `}>
+                        {msg.image ? (
+                            <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
+                        ) : (
+                            <p >{msg.text}</p>
+                        )}
+                    </div>
+                ))}
             </div>
         </div>
     ) : (
